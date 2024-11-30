62c299675f26f303c41f1c57a4c4d554d02243ac
Shubham Lasre <shubham.lasre@ibm.com>
2024-12-01 00:04:57 +0530

    refactored ui folders

---
+++ library_ui/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {LibraryUI} from './components/LibraryUI/LibraryUI';
+
+function App() {
+  return (
+    <div>
+      <LibraryUI/>
+    </div>
+  );
+}
+
+export default App;
---
+++ library_ui/src/components/LibraryUI/LibraryUI.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LibraryUI = () => {
+  return (
+    <div>LibraryUI</div>
+  )
+}
